ee48c3f728d9d6d3d7ed2c5290d2287167d2b29a
Mounika <mounika.jothi@gmail.com>
2023-05-22 15:36:57 -0700

    updating code for hw4.2

--- class_4_scenario_outline/features/steps/amazon_addproduct_hw4.2.py
+++ class_4_scenario_outline/features/steps/amazon_addproduct_hw4.2.py
@@ -7,6 +7,9 @@ SEARCH_SUBMIT = (By.ID, 'nav-search-submit-button')
 ADD_TO_CART = (By.ID, 'add-to-cart-button')
 CART_BTN = (By.ID, 'nav-cart-count')
 SUB_TOTAL_CART = (By.ID, 'sc-subtotal-label-activecart')
+ALL_PRODUCTS = (By.XPATH,
+                "//div[@data-component-type='s-search-result']//a[@class='a-link-normal s-underline-text s-underline-link-text s-link-style a-text-normal']")
+
 
 @when('search the item coffee')
 def search_item(context):
@@ -16,8 +19,8 @@ def search_item(context):
 
 @then('Add the coffee link to cart')
 def add_item_to_cart(context):
-    context.driver.find_element(By.CSS_SELECTOR,
-                                "div.a-section img.s-image[alt='Sponsored Ad - AmazonFresh Organic Fair Trade Sumatra Ground Coffee, Dark Roast, 12 Ounce']").click()
+    all_prod = context.driver.find_elements(*ALL_PRODUCTS)
+    all_prod[0].click()
     sleep(5)
     context.driver.find_element(*ADD_TO_CART).click()
 
@@ -29,10 +32,7 @@ def click_cart(context):
 
 @then('Verify the coffee is added successfully in cart')
 def verifying_cart(context):
-    assert context.driver.find_element(By.XPATH,
-                                       "//span[@class='a-truncate-cut' and contains(text(),'AmazonFresh Organic Fair Trade Sumatra Ground Coffee,')]").is_displayed(),\
-        f'Error!!, Item is not added in cart'
-    print('Item added successfully')
     quantity = context.driver.find_element(*SUB_TOTAL_CART).text
     print(f'Number of items in cart is {quantity}')
-
+    assert quantity != 0, f'Error!!, Item is not added in cart'
+    print('Item added successfully')
